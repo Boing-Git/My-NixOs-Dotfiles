37b4398187abaac935dc13f82b4dc602999f96cb
Boing <boing@nixos.local>
2026-06-05 01:38:29 +0530

    Update system configuration

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,14 +1,43 @@
 { config, pkgs, inputs, ... }:
 
 {
-  # Enable the Hyprland window manager
-  programs.hyprland.enable = true;
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Config Fish
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+    set -gx LANG en_US.UTF-8
+    set -gx LC_ALL en_US.UTF-8
+    '';
+  };
+  
+  # Config hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  
+    # Correct packages for Nvidia hardware acceleration inside Steam FHS
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      vkd3d-proton         # Correct package for DX12 to Vulkan translation
+      proton-ge-bin        # (Optional but recommended) includes built-in NVAPI fixes
+    ];
+  };
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
+    hyperhdr
     git
     vscodium
     foot
@@ -21,6 +50,21 @@
     eza
     zoxide
     nerd-fonts.jetbrains-mono
+    wl-clipboard
+    cliphist
+    xdg-utils
+    gtk3
+    desktop-file-utils
+    cairo
+    pango
+    gobject-introspection
+
+    (python3.withPackages (ps: with ps; [
+      tkinter
+      pygobject3
+      pywebview
+      flask
+    ]))
 
     # Core Build & Framework Tools
     cmake
