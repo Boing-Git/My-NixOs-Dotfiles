4ebecdcf8c446e2e7c6589d40cf8eb1df04cfed1
Boing <boing@nixos.local>
2026-06-05 01:58:36 +0530

    First ever commit done by me the user not the ai

--- modules/HM/PackConfig.nix
+++ modules/HM/PackConfig.nix
@@ -1,13 +1,11 @@
 # Providing things to do stuff with
 { config, pkgs, lib, inputs, ... }:
 
-# Pulling spicetify packages from the flake input for this system
-let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
-in
+
 {
   imports = [
     ./LPackConfigs/starship.nix
+    ./LPackConfigs/foot.nix
   ];
 
   # Caelestia shell and CLI - the main desktop environment
@@ -25,6 +23,10 @@ in
 
   # Spicetify - Spotify client customization with Catppuccin Mocha
   programs.spicetify = {
+    # Pulling spicetify packages from the flake input for this system
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+    in
     enable = true;
     enabledCustomApps = with spicePkgs.apps; [
       lyricsPlus        # Lyrics panel inside Spotify
@@ -35,53 +37,15 @@ in
       beautifulLyrics   # Glowing ambient lyrics
       popupLyrics       # Floating lyrics window
     ];
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+    theme = spicePkgs.themes.text;
+    colorScheme = "spotify";
   };
 
-  # Foot terminal - main terminal emulator with JetBrains Nerd Font
-  programs.foot = {
-    enable = true;
-    settings = {
-      main = {
-        shell = "fish";
-        title = "foot";
-        font = "JetBrains Mono Nerd Font:size=12";
-        letter-spacing = 0;
-        dpi-aware = "no";
-        pad = "25x25";
-        bold-text-in-bright = "no";
-        gamma-correct-blending = "no";
-      };
-      # How far back I can scroll in the terminal
-      scrollback = {
-        lines = 10000;
-      };
-      # Thin beam cursor instead of block
-      cursor = {
-        style = "beam";
-        beam-thickness = "1.5";
-      };
-      # Terminal background transparency
-      colors-dark = {
-        alpha = "0.78";
-      };
-      # Scrollback and search shortcuts
-      key-bindings = {
-        scrollback-up-page = "Page_Up";
-        scrollback-down-page = "Page_Down";
-        search-start = "Control+Shift+f";
-      };
-      search-bindings = {
-        cancel = "Escape";
-        find-prev = "Shift+F3";
-        find-next = "F3 Control+G";
-      };
-    };
+  # Bulk enabling programs
+  programs = {
+    home-manager.enable = true;
+    zen-browser.enable = true;
+    git.enable = true;
+    steam.enable = true;
   };
-
-  # Enabling home-manager, kitty and zen browser
-  programs.home-manager.enable = true;
-  programs.kitty.enable = true;
-  programs.zen-browser.enable = true;
 }
