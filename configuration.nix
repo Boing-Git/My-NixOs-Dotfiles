ad9b82bce29922ce3fe8725017f8d09cca7ef930
Boing <boing@nixos.local>
2026-06-05 03:12:19 +0530

    Bulk import nix file from a modules folder

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../hardware-configuration.nix
-      ../modules/programs.nix
+      ./hardware-configuration.nix
+      ./modules/programs.nix
     ];
 
   # Bootloader.
