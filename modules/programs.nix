cde29195fc7577104afe2b35cb0793749fcf278d
Boing <boing@nixos.local>
2026-06-05 02:51:03 +0530

    having fun with fish shell

--- modules/programs.nix
+++ modules/programs.nix
@@ -11,7 +11,9 @@
     set -gx LANG en_US.UTF-8
     set -gx LC_ALL en_US.UTF-8
     fastfetch
-    set -g fish_greeting "                 >_<"
+    set -g fish_greeting "                 
+    >_<
+    "
     '';
   };
   
