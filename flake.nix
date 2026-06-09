5225b26e580faad1b0ad295ea994405fa26bd283
Boing <boing@nixos.local>
2026-06-10 00:43:50 +0530

    changing to zen twilight to beta

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.backupFileExtension = "backup";
           
           home-manager.users.boing = {
             imports = [
