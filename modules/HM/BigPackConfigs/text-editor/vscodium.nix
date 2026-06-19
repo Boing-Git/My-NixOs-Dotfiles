3e1a8637827236447df4a66ce80f53106b47da53
Boing <boing@nixos.local>
2026-06-19 16:53:16 +0530

    converting my every settings for vscodium into vscodium.nix

--- modules/HM/BigPackConfigs/text-editor/vscodium.nix
+++ modules/HM/BigPackConfigs/text-editor/vscodium.nix
@@ -1,7 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  # 1. Your standard VSCodium configuration (without the custom extension in the list)
   programs.vscodium = {
     enable = true;
     package = pkgs.vscodium;
@@ -14,16 +13,52 @@
         mvllow.rose-pine
         ravenothere.rose-pine-symbols
         theqtcompany.qt-qml
-        # The official Lua extension from sumneko
         sumneko.lua
       ];
+
+      # Merging your keybindings here
+      keybindings = [
+        { key = "ctrl+shift+alt+r"; command = "workbench.action.reloadWindow"; }
+        { key = "ctrl+pageup"; command = "workbench.action.previousEditor"; }
+        { key = "ctrl+pagedown"; command = "workbench.action.nextEditor"; }
+        { key = "ctrl+shift+up"; command = "editor.action.moveLinesUpAction"; when = "editorTextFocus && !editorReadonly"; }
+        { key = "ctrl+shift+down"; command = "editor.action.moveLinesDownAction"; when = "editorTextFocus && !editorReadonly"; }
+        { key = "shift+alt+up"; command = "editor.action.insertCursorAbove"; when = "editorTextFocus"; }
+        { key = "shift+alt+down"; command = "editor.action.insertCursorBelow"; when = "editorTextFocus"; }
+        { key = "shift+h"; command = "workbench.action.previousEditor"; when = "editorTextFocus && vim.active && vim.mode == 'Normal'"; }
+        { key = "shift+l"; command = "workbench.action.nextEditor"; when = "editorTextFocus && vim.active && vim.mode == 'Normal'"; }
+        { key = "j"; command = "list.focusDown"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView'"; }
+        { key = "k"; command = "list.focusUp"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView'"; }
+        { key = "h"; command = "list.collapse"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView'"; }
+        { key = "l"; command = "list.select"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView'"; }
+        { key = "a"; command = "explorer.newFile"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView' && !inputFocus"; }
+        { key = "shift+a"; command = "explorer.newFolder"; when = "filesToExplore && focusedView == 'workbench.explorer.fileView' && !inputFocus"; }
+        { key = "a"; command = "explorer.newFile"; when = "explorerViewletFocus && !inputFocus"; }
+        { key = "shift+a"; command = "explorer.newFolder"; when = "explorerViewletFocus && !inputFocus"; }
+        { key = "ctrl+shift+q"; command = "workbench.action.closeActiveEditor"; when = "editorTextFocus"; }
+      ];
+
+      # Adding your files.exclude configuration seen in image_47d02d.jpg
+      userSettings = {
+        "files.exclude" = {
+          "**/.git" = true;
+          "**/.svn" = true;
+          "**/.hg" = true;
+          "**/.DS_Store" = true;
+          "**/Thumbs.db" = true;
+          "**/.vscode" = true;
+          "**/stubs" = true;
+          "**/.luarc.json" = true;
+          "**/flake.lock" = true;
+          "**/LICENSE" = true;
+        };
+      };
     };
   };
 
-  # 2. Place the activation script here, right next to programs.vscodium
+  # Activation script for the VSIX extension
   home.activation = {
     installCaelestiaExtension = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      # Check if the extension is missing, and install it mutably if so
       if ! ${pkgs.vscodium}/bin/codium --list-extensions | grep -iq "soramanew.caelestia-vscode-integration"; then
         ${pkgs.vscodium}/bin/codium --install-extension ${./caelestia-vscode-integration-1.2.0.vsix} --force
       fi
