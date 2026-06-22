d8af4d1cf1dc23ce76142cf09bda41788442c9e5
Boing <boing@nixos.local>
2026-06-23 03:45:35 +0530

    Fixed the path in the matugen.nix

--- modules/HM/BigPackConfigs/Matugen/Matugen.nix
+++ modules/HM/BigPackConfigs/Matugen/Matugen.nix
@@ -11,31 +11,31 @@
     templates = {
       foot = {
         input_path = "./Templates/footTheme.ini";
-        output_path = ".config/foot/theme.ini";
+        output_path = "${config.xdg.configHome}/foot/theme.ini";
       };
       gtk = {
         input_path = "./Templates/gtkColors.css";
-        output_path = ".config/gtk-colors/gtkColors.css";
+        output_path = "${config.xdg.configHome}/gtk-colors/gtkColors.css";
       };
       hypr = {
         input_path = "./Templates/hypr-colors.lua";
-        output_path = ".config/hypr/colors.conf";
+        output_path = "${config.xdg.configHome}/hypr/scheme/material-you.conf";
       };
       qt = {
         input_path = "./Templates/qtColors.conf";
-        output_path = ".config/qt5ct/colors/matugen.conf";
+        output_path = "${config.xdg.configHome}/qt5ct/colors/matugen.conf";
       };
       quickshell = {
         input_path = "./Templates/quickshellColors.js";
-        output_path = ".config/quickshell/Variables/colors.js";
+        output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js";
       };
       starship = {
         input_path = "./Templates/starship.toml";
-        output_path = ".config/starship/starship.toml";
+        output_path = "${config.xdg.configHome}/starship/starship.toml";
       };
       vscodium = {
         input_path = "./Templates/vscodium.json";
-        output_path = ".config/vscodium/theme-colors.json";
+        output_path = "${config.xdg.configHome}/vscodium/theme-colors.json";
       };
     };
   };
