f4c229fee0bf1913c22c85efff9af66e232e0c26
Boing <boing@nixos.local>
2026-06-23 02:52:23 +0530

    Made matugen templates for qt,gtk,vscodium,hyprland fixed the gtk.nix time 3 fixed vscodium.nix time 2

--- modules/HM/BigPackConfigs/text-editor/vscodium.nix
+++ modules/HM/BigPackConfigs/text-editor/vscodium.nix
@@ -51,7 +51,7 @@
 
       userSettings = {
         "workbench.colorCustomizations" = {
-          "import": "${config.xdg.configHome}/vscodium/colors.json"
+          "import" = "${config.xdg.configHome}/vscodium/colors.json"
         }
         "workbench.statusBar.visible" = false;
         "window.menuBarVisibility" = "hidden";
