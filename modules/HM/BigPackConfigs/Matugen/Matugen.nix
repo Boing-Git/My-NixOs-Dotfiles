e1828c6f2aae8d631293cbd5e0ad9002907e3f97
Boing <boing@nixos.local>
2026-06-23 05:01:20 +0530

    Stopped copying the templates in the nixos config dir with home manager time 2

--- modules/HM/BigPackConfigs/Matugen/Matugen.nix
+++ modules/HM/BigPackConfigs/Matugen/Matugen.nix
@@ -1,46 +1,45 @@
 { config, pkgs, inputs, ... }: {
-  # 1. Install the CLI tool so it's in your PATH
   home.packages = [ inputs.matugen.packages.${pkgs.system}.default ];
 
-  # 2. Copy your templates folder into ~/.config/matugen/templates
-  xdg.configFile."matugen/templates".source = ./Templates;
+  # Force the creation of the config file without managing the entire directory
+  home.file.".config/matugen/config.toml" = {
+    force = true; 
+    text = ''
+      [config]
+      variant = "dark"
+      json_format = "hex"
 
-  # 3. Generate the actual config.toml that the CLI relies on
-  xdg.configFile."matugen/config.toml".text = ''
-    [config]
-    variant = "dark"
-    json_format = "hex"
+      [templates.foot]
+      input_path = "${config.xdg.configHome}/matugen/templates/footTheme.ini"
+      output_path = "${config.xdg.configHome}/foot/theme.ini"
 
-    [templates.foot]
-    input_path = "${config.xdg.configHome}/matugen/templates/footTheme.ini"
-    output_path = "${config.xdg.configHome}/foot/theme.ini"
+      [templates.gtk]
+      input_path = "${config.xdg.configHome}/matugen/templates/gtkColors.css"
+      output_path = "${config.xdg.configHome}/gtk-colors/gtkColors.css"
 
-    [templates.gtk]
-    input_path = "${config.xdg.configHome}/matugen/templates/gtkColors.css"
-    output_path = "${config.xdg.configHome}/gtk-colors/gtkColors.css"
+      [templates.hypr]
+      input_path = "${config.xdg.configHome}/matugen/templates/hypr-colors.lua"
+      output_path = "${config.xdg.configHome}/hypr/scheme/material-you.conf"
 
-    [templates.hypr]
-    input_path = "${config.xdg.configHome}/matugen/templates/hypr-colors.lua"
-    output_path = "${config.xdg.configHome}/hypr/scheme/material-you.conf"
+      [templates.qt5]
+      input_path = "${config.xdg.configHome}/matugen/templates/qtColors.conf"
+      output_path = "${config.xdg.configHome}/qt5ct/colors/matugen.conf"
 
-    [templates.qt5]
-    input_path = "${config.xdg.configHome}/matugen/templates/qtColors.conf"
-    output_path = "${config.xdg.configHome}/qt5ct/colors/matugen.conf"
+      [templates.qt6]
+      input_path = "${config.xdg.configHome}/matugen/templates/qtColors.conf"
+      output_path = "${config.xdg.configHome}/qt6ct/colors/matugen.conf"
 
-    [templates.qt6]
-    input_path = "${config.xdg.configHome}/matugen/templates/qtColors.conf"
-    output_path = "${config.xdg.configHome}/qt6ct/colors/matugen.conf"
+      [templates.quickshell]
+      input_path = "${config.xdg.configHome}/matugen/templates/quickshellColors.js"
+      output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js"
 
-    [templates.quickshell]
-    input_path = "${config.xdg.configHome}/matugen/templates/quickshellColors.js"
-    output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js"
+      [templates.starship]
+      input_path = "${config.xdg.configHome}/matugen/templates/starship.toml"
+      output_path = "${config.xdg.configHome}/starship/starship.toml"
 
-    [templates.starship]
-    input_path = "${config.xdg.configHome}/matugen/templates/starship.toml"
-    output_path = "${config.xdg.configHome}/starship/starship.toml"
-
-    [templates.vscodium]
-    input_path = "${config.xdg.configHome}/matugen/templates/vscodium.json"
-    output_path = "${config.xdg.configHome}/vscodium/theme-colors.json"
-  '';
+      [templates.vscodium]
+      input_path = "${config.xdg.configHome}/matugen/templates/vscodium.json"
+      output_path = "${config.xdg.configHome}/vscodium/theme-colors.json"
+    '';
+  };
 }
