cbf5ceddfe8409770812c6fc2ce9712415e8cd78
Boing <boing@nixos.local>
2026-06-22 20:24:23 +0530

    Changing the path for the quickshellColors.js from Colors/colors.js to Variables/colors.js

--- modules/HM/BigPackConfigs/Matugen/Matugen.nix
+++ modules/HM/BigPackConfigs/Matugen/Matugen.nix
@@ -24,7 +24,7 @@ in
       };
       quickshell = {
         input_path = "${config.xdg.configHome}/matugen/Templates/quickshellColors.js";
-        output_path = "${config.xdg.configHome}/quickshell/Colors/colors.js";
+        output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js";
       };
     };
   };
