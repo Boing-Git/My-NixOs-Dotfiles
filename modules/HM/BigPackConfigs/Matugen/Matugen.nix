6ed2bd49ec7f778f9d35d8bb3417296e3bf12e7f
Boing <boing@nixos.local>
2026-06-23 03:09:24 +0530

    Fixing the matugen.nix

--- modules/HM/BigPackConfigs/Matugen/Matugen.nix
+++ modules/HM/BigPackConfigs/Matugen/Matugen.nix
@@ -1,55 +1,46 @@
-{ config, pkgs, inputs, ... }:
-
-let
-  tomlFormat = pkgs.formats.toml { };
-in
-{
-  home.packages = [
-    inputs.matugen.packages.${pkgs.system}.default
+{ config, pkgs, inputs, ... }: {
+  imports = [
+    inputs.matugen.nixosModules.default # Or homeManagerModules.default depending on your setup
   ];
 
-  # 1. THIS IS THE MISSING PIECE: Copy your Templates folder into ~/.config/matugen/Templates
-  xdg.configFile."matugen/Templates".source = ./Templates;
+  programs.matugen = {
+    enable = true;
+    variant = "dark";
+    jsonFormat = "hex";
+    palette = "default";
 
-  xdg.configFile."matugen/config.toml".source = tomlFormat.generate "matugen-config" {
-    config = {
-      variant = "dark";
-      json_format = "hex";
-    };
-
+    # The module generates these templates into a read-only store directory
     templates = {
-      foot = {
-        input_path = "./Templates/footTheme.ini";
-        output_path = "${config.xdg.configHome}/foot/theme.ini";
+      ags = {
+        input_path = "./templates/ags.scss";
+        output_path = ".config/ags/scss/colors.scss";
       };
-      quickshell = {
-        input_path = "./Templates/quickshellColors.js";
-        output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js";
+      kitty = {
+        input_path = "./templates/kitty.conf";
+        output_path = ".config/kitty/colors.conf";
       };
       gtk = {
-        input_path = "./Templates/gtkColors.css";
-        output_path = "${config.xdg.configHome}/gtk-colors/gtkColors.css";
+        input_path = "./templates/gtk.css";
+        output_path = ".config/gtk-4.0/gtk.css";
       };
-      # Qt5 Configuration Output
-      qt5ct-colors = {
-        input_path = ./templates/qtct.conf;
-        output_path = "${config.xdg.configHome}/qt5ct/colors/matugen.conf";
+      hypr = {
+        input_path = "./templates/hypr.conf";
+        output_path = ".config/hypr/colors.conf";
       };
-      
-      # Qt6 Configuration Output
-      qt6ct-colors = {
-        input_path = ./templates/qtct.conf;
-        output_path = "${config.xdg.configHome}/qt6ct/colors/matugen.conf";
-      };
-
-      starship = {
-        input_path = ./Templates/starship.toml;
-        output_path = "${config.xdg.configHome}/starship/starship.toml";
-      };
-      vscodium = {
-        input_path = ./Templates/vscodium.json;
-        output_path = "${config.xdg.configHome}/vscodium/vscodium.json";
+      yazi = {
+        input_path = "./templates/yazi.toml";
+        output_path = ".config/yazi/theme.toml";
       };
     };
+  };
+
+  # Now, link the generated files from the matugen store path to your actual config directories
+  # as shown in image_aa5b45.png
+  home.configFile = {
+    "ags/scss/colors.scss".source = "${config.programs.matugen.theme.files}/.config/ags/scss/colors.scss";
+    "kitty/colors.conf".source = "${config.programs.matugen.theme.files}/.config/kitty/colors.conf";
+    "gtk-4.0/gtk.css".source = "${config.programs.matugen.theme.files}/.config/gtk-4.0/gtk.css";
+    "hypr/colors.conf".source = "${config.programs.matugen.theme.files}/.config/hypr/colors.conf";
+    "yazi/theme.toml".source = "${config.programs.matugen.theme.files}/.config/yazi/theme.toml";
   };
 }
