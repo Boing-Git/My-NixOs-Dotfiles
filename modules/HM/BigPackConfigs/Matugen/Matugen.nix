a28d91d50451e90772cfbe5e26e4121f488be971
Boing <boing@nixos.local>
2026-06-23 04:32:59 +0530

    Fixing the matugen time 1 after reboot

--- modules/HM/BigPackConfigs/Matugen/Matugen.nix
+++ modules/HM/BigPackConfigs/Matugen/Matugen.nix
@@ -1,40 +1,42 @@
-# Save as: ./modules/HM/matugen.nix
 { config, pkgs, inputs, ... }: {
   imports = [
-    # Use the home manager module variant to correctly resolve user xdg paths
     inputs.matugen.homeManagerModules.default 
   ];
 
   programs.matugen = {
     enable = true;
+    variant = "dark";
+    jsonFormat = "hex";
 
     templates = {
       foot = {
-        input_path = "./Templates/footTheme.ini";
+        # Using ${...} without quotes around the path copies the file to the Nix store
+        # and provides the absolute path to the Matugen config.
+        input_path = "${./Templates/footTheme.ini}";
         output_path = "${config.xdg.configHome}/foot/theme.ini";
       };
       gtk = {
-        input_path = "./Templates/gtkColors.css";
+        input_path = "${./Templates/gtkColors.css}";
         output_path = "${config.xdg.configHome}/gtk-colors/gtkColors.css";
       };
       hypr = {
-        input_path = "./Templates/hypr-colors.lua";
+        input_path = "${./Templates/hypr-colors.lua}";
         output_path = "${config.xdg.configHome}/hypr/scheme/material-you.conf";
       };
       qt = {
-        input_path = "./Templates/qtColors.conf";
+        input_path = "${./Templates/qtColors.conf}";
         output_path = "${config.xdg.configHome}/qt5ct/colors/matugen.conf";
       };
       quickshell = {
-        input_path = "./Templates/quickshellColors.js";
+        input_path = "${./Templates/quickshellColors.js}";
         output_path = "${config.xdg.configHome}/quickshell/Variables/colors.js";
       };
       starship = {
-        input_path = "./Templates/starship.toml";
+        input_path = "${./Templates/starship.toml}";
         output_path = "${config.xdg.configHome}/starship/starship.toml";
       };
       vscodium = {
-        input_path = "./Templates/vscodium.json";
+        input_path = "${./Templates/vscodium.json}";
         output_path = "${config.xdg.configHome}/vscodium/theme-colors.json";
       };
     };
