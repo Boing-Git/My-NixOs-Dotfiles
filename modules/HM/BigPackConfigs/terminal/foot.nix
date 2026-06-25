ba352ed994ffe986ce445c0a7ebab33d487454d6
Boing <boing@nixos.local>
2026-06-26 03:56:27 +0530

    Included the theme.ini for foot fixed errors time 2 fixed the way the file was included

--- modules/HM/BigPackConfigs/terminal/foot.nix
+++ modules/HM/BigPackConfigs/terminal/foot.nix
@@ -5,7 +5,7 @@
   programs.foot = {
     enable = true;
     settings = {
-      include=~/.config/foot/theme.ini
+      include=~/.config/foot/theme.ini;
       main = {
         shell = "fish";
         title = "foot";
