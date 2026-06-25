d7d63c897f868ab110e16d5c8c0c2136f3cb5560
Boing-Git <jivanmatoo@gmail.com>
2026-06-25 18:46:10 +0530

    Update install.sh
    fixed some issuses with the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 git clone https://github.com/Boing-Git/My-NixOs-Dotfiles ~/Nixos
 cd ~/Nixos
 
 cp -r ~/Nixos/quickshell ~/.config/quickshell
+
+cp /etc/nixos/hardware-configuration.nix ./
+
+sudo nixos-rebuild switch --flake .#nixos
