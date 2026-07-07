edcc6873b9dd0d346d843b812431b7ba25d4bda5
Boing <boing@nixos.local>
2026-07-07 17:57:07 +0530

    Fixing rebuilding errors time 4

--- modules/system/desktop/greetd.nix
+++ modules/system/desktop/greetd.nix
@@ -3,13 +3,13 @@
 {
   # ── Greeter (greetd + quickshell) ──────────────────────────────────────
   environment.etc."quickshell-greeter/shell.qml".source =
-    /home/boing/.config/quickshell/greeter/shell.qml;
+    "/home/boing/dotfiles/quickshell/greeter/shell.qml";
   environment.etc."quickshell-greeter/LoginCard.qml".source =
-    /home/boing/.config/quickshell/greeter/LoginCard.qml;
+    "/home/boing/dotfiles/quickshell/greeter/LoginCard.qml";
   environment.etc."quickshell-greeter/Variables/Theme.qml".source =
-    /home/boing/.config/quickshell/greeter/Variables/Theme.qml;
+    "/home/boing/dotfiles/quickshell/greeter/Variables/Theme.qml";
   environment.etc."quickshell-greeter/Variables/variables.js".source =
-    /home/boing/.config/quickshell/greeter/Variables/variables.js;
+    "/home/boing/dotfiles/quickshell/greeter/Variables/variables.js";
   environment.etc."quickshell-greeter/qmldir".text = ''
     module QuickshellGreeter
     singleton Theme 1.0 Variables/Theme.qml
