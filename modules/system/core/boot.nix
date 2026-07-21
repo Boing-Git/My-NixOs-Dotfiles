04ba8d658fc16e33639481d7d2f6ee595b5de15e
Boingly <BoingDoing@gmail.com>
2026-07-21 12:46:00 +0530

    Fixing the micro stutters in game

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -8,7 +8,12 @@
     "nvidia-drm.modeset=1"
     "nvidia-drm.fbdev=1"
     "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "pcie_aspm=off"
   ];
+  boot.extraModprobeConfig = ''
+    options rtw88_core disable_aspm=y
+    options rtl8821ae aspm=0
+  '';
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "ntsync" ];
   boot.kernel.sysctl = {
