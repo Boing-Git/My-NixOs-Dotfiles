580854c538c56d9c97d6daf56ba4d19b486516ee
Boingly <BoingDoing@gmail.com>
2026-08-05 23:31:19 +0530

    Removed the reonly parameters

--- modules/system/programs/maccel.nix
+++ modules/system/programs/maccel.nix
@@ -4,12 +4,5 @@
   hardware.maccel = {
     enable = true;
     enableCli = true; # Enables command-line tool for runtime parameter adjustment
-    parameters = {
-      mode = "linear";          # "linear" or other RawAccel-like curves
-      sensMultiplier = 1.0;     # Your baseline sensitivity modifier
-      acceleration = 0.3;       # Acceleration rate
-      offset = 2.0;             # Input offset in raw counts
-      outputCap = 2.0;          # Cap on the final acceleration multiplier
-    };
   };
 }
