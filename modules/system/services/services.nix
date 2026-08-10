458a357c93e911da2ddd5b21ebd190af5d4fa297
Boingly <BoingDoing@gmail.com>
2026-08-10 17:18:06 +0530

    Configured keyd too

--- modules/system/services/services.nix
+++ modules/system/services/services.nix
@@ -13,7 +13,27 @@ in {
     security.polkit.enable = true;
     programs.coolercontrol.enable = true;
 
-    services.keyd.enable = true;
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "*" ];
+          settings = {
+            main = {
+              # Hold 'alt' to act as 'meta' (Super).
+              # Tap 'alt' once to briefly enter the 'alt_double_tap' layer.
+              alt = "overload(meta, oneshot(alt_double_tap))";
+            };
+            
+            alt_double_tap = {
+              # What happens when you hit 'alt' a second time:
+              # layer(control) means you tap once, then press and hold the second time to act as Control.
+              alt = "layer(control)"; 
+            };
+          };
+        };
+      };
+  };
 
     services.pipewire = {
       enable = true;
